167f2938dda6d30f33cece3f1927a04e5068218f
Anderson Batista Ferreira da Silva <andersonbfs15@gmail.com>
2021-08-07 16:39:07 -0300

    Finalizando classe de dominio

--- src/main/java/com/anderson/ToDo/domain/ToDo.java
+++ src/main/java/com/anderson/ToDo/domain/ToDo.java
@@ -1,10 +1,22 @@
 package com.anderson.ToDo.domain;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class ToDo {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class ToDo implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String titulo;
 	private String descricacao;
 	private LocalDateTime dataParaFinalizar;
